db3d22b8327449e26a8baa6335d6fb8701aaa166
fgdamasceno <fgdamasceno@gmail.com>
2025-04-15 16:17:02 -0300

    Screen categoria+restaurantes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:techtaste/data/restaurant_data.dart';
 import 'package:techtaste/ui/_core/app_theme.dart';
 import 'package:techtaste/ui/splash/splash_screen.dart';
 
-void main() {
-  runApp(MyApp()); // Instancia a classe MyApp
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  RestaurantData restaurantData = RestaurantData();
+  await restaurantData.getRestaurants();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) {
+            return restaurantData;
+          },
+        ),
+      ],
+      child: MyApp(),
+    ),
+  ); // Instancia a classe MyApp
 }
 
 // Cria a classe MyApp
--- lib/ui/_core/app_colors.dart
+++ lib/ui/_core/app_colors.dart
@@ -4,4 +4,5 @@ import 'package:flutter/widgets.dart';
 abstract class AppColors {
   static const Color backgroundColor = Color(0xFF202123);
   static const Color mainColor = Color(0xFFFFA559);
+  static const Color lightBackgroundColor = Color(0xFF343541);
 }
